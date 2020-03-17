7672bd6c14ba1116faa5b217735a24a05d9b4fd3
Samuel <s1810307070@students.fh-hagenberg.at>
2020-03-17 14:36:13 +0100

    changed calculation

--- FibonacciSeries/Program.cs
+++ FibonacciSeries/Program.cs
@@ -15,7 +15,7 @@ namespace FibonacciSeries {
     static int[] CreateFibonacciSeries(int count) {
       int[] fibonacci = new int[count];
       fibonacci[0] = 0;
-      fibonacci[1] = 1;
+      fibonacci[1] = 3;
 
       for (int i = 2; i < count; i++) {
         fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
